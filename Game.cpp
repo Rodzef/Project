bc0d5afab495dea935a55e912388bcaae5b084a4
Rodzef <71638251+Rodzef@users.noreply.github.com>
2020-09-22 12:10:03 +0300

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,10 @@
+#include <poorpch.h>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+template<class T>
+void show(const T message)
+{
+	std::cout << message << std::endl;
+}
