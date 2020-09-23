baff8809bfc197806d154ff72360efa009f13b30
Rodzef <71638251+Rodzef@users.noreply.github.com>
2020-09-23 18:47:20 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -5,26 +5,27 @@
 #include "Base.h"
 
 int main(int argc, char* argv[]);
-
+// Cоздаем класс игры
 class Game
 {
 public:
 	Game()
 	{
-		window.create(sf::VideoMode(800, 600), "15 Puzzle");
+		//Открываем окошко игры, подргужаем файл с числами, шрифт, цвет выбираем шрифта и размер
+		window.create(sf::VideoMode(500, 500), "15 Puzzle");
 		window.setVerticalSyncEnabled(true);
 
 		texture.loadFromFile("assets/numbers.png");
 		font.loadFromFile("assets/Montserrat-Regular.otf");
 		text.setFont(font);
-		text.setFillColor(sf::Color::White);
+		text.setFillColor(sf::Color::Green);
 		text.setCharacterSize(20);
-		text.setString("Space - to restart");
-		text.setPosition({ static_cast<float>(window.getSize().x - 250.0f), 100.0f });
+		text.setString("F2 - to restart");
+		text.setPosition({ static_cast<float>(100), 30.0f });
 
 		level.Load(texture, font, { { 32, 32 }, {2.0f, 2.0f }, { 10.0f, 10.0f } });
 	}
-
+	// Деструктор
 	~Game()
 	{
 		window.close();
@@ -36,6 +37,7 @@ private:
 
 	void Run()
 	{
+		// Обработка времени
 		sf::Time pastTime;
 		sf::Time deltaTime;
 		sf::Clock clock;
@@ -53,6 +55,8 @@ private:
 			}
 
 			OnUpdate(deltaTime.asSeconds());
+			
+			//Цввет окошка
 
 			window.clear(sf::Color(
 				static_cast<sf::Uint8>(0.1f * 255.0f),
@@ -78,15 +82,16 @@ private:
 
 	void OnEvent(sf::Event& e)
 	{
+		// Считывание команды (перезапуск игры, закрытие)
 		switch (e.type)
 		{
 		case sf::Event::Closed:
-			OnWindowClose(e);
+			OnWindowClose(e); // закрытие окна
 		case sf::Event::KeyReleased:
-			if (e.key.code == sf::Keyboard::Space)
-				level.Restart();
+			if (e.key.code == sf::Keyboard::F2)
+				level.Restart(); // рестарт
 			if (e.key.code == sf::Keyboard::Escape)
-				running = false;
+				running = false; // закрытие
 		}
 
 		level.OnEvent(e);
@@ -98,10 +103,10 @@ private:
 
 		level.OnGUI(window);
 	}
-
+	// то что закрывает окно
 	void OnWindowClose(sf::Event& e)
 	{
-		running = false;
+		running = false; 
 	}
 
 private:
@@ -112,5 +117,5 @@ private:
 	sf::Text text;
 	Level level;
 
-	std::string restartText = "Space - to restart";
+	std::string restartText = "F2 - to restart";
 };
