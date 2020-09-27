ed4ed8e444211fd279b16a3c0ff80e085d9f0c57
Rodzef <71638251+Rodzef@users.noreply.github.com>
2020-09-27 21:59:20 +0300

    Delete Random.cpp

--- Random.cpp
+++
@@ -1,5 +0,0 @@
-#include "X.h"
-#include "Random.h"
-
-std::mt19937 Random::s_Engine = std::mt19937(std::random_device()());
-std::uniform_real_distribution<float> Random::s_Distribution(0.0f, 1.0f);
